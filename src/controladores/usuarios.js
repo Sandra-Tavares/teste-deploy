37f5b810b4a569ba26866dc17096080a95deac16
Sandra Tavares <sandratavaress1972@gmail.com>
2023-11-27 20:39:15 -0300

    atualização

---
+++ src/controladores/usuarios.js
@@ -0,0 +1,80 @@
+const knex = require('../conexao');
+const bcrypt = require('bcrypt');
+
+const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha, telefone } = req.body;
+
+    if (!nome) {
+
+        return res.status(404).json("O campo nome é obrigatório");
+    }
+
+
+    if (!email) {
+        return res.status(404).json("O campo email é obrigatório");
+    }
+
+    if (!senha) {
+        return res.status(404).json("O campo senha é obrigatório");
+    }
+
+    if (!telefone) {
+        return res.status(404).json("O campo nome_loja é obrigatório");
+    }
+
+    try {
+        const emailExiste = await knex('usuarios').where({ email }).first
+
+        if (emailExiste) {
+            return res.status(400).json("O email já existe");
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        const usuario = await knex('usuarios').insert({ nome, email, senha: senhaCriptografada, nome_loja }).returning('*')
+
+        if (!usuario) {
+            return res.status(404).json("O usuário não foi cadastrado.");
+        }
+
+        return res.status(200).json(usuario[0]);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const obterPerfil = async (req, res) => {
+    return res.status(200).json(req.usuario);
+}
+
+const atualizarPerfil = async (req, res) => {
+    const { nome, email, senha, telefone } = req.body;
+
+    if (!nome && !email && !senha && !telefone) {
+        return res.status(404).json('É obrigatório informar ao menos um campo para atualização');
+    }
+
+    try {
+
+        const emailExiste = await knex('usuarios').where({ email }).first
+
+        if (emailExiste && emailExiste != req.usuario.email) {
+            return res.status(400).json("O email já existe");
+        }
+        const usuarioAtualizado = await knex('usuarios').where({ id: req.isuario.id }).update({ nome, email, senha: senhaCriptografada, nome_loja })
+
+        if (!usuarioAtualizado) {
+            return res.status(400).json("O usuario não foi atualizado");
+        }
+
+        return res.status(200).json('Usuario foi atualizado com sucesso.');
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+module.exports = {
+    cadastrarUsuario,
+    obterPerfil,
+    atualizarPerfil
+}
